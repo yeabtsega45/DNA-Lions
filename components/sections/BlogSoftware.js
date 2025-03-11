f425915c5ea715a38dbe8b311be63fe8e3c7027e
yeabtsega45 <yabtsega2022@gmail.com>
2025-03-11 14:16:54 +0300

    update services page content

--- components/sections/BlogSoftware.js
+++ components/sections/BlogSoftware.js
@@ -18,7 +18,7 @@ function BlogSoftware() {
                 />
                 <a href="#" className="categories">
                   <i className="icon-folder" />
-                  EMPLOYEE RELATIONS
+                  SERVICES
                 </a>
               </div>
               <div className="content_box">
@@ -28,12 +28,17 @@ function BlogSoftware() {
                 <div className="source">
                   <h2 className="title">
                     <a href="blog-single.html" rel="bookmark">
-                      Workplace problems in your business?
+                      Build Business Credit (No PG)
                     </a>
                   </h2>
                   <p className="short_desc">
-                    On the other hand, we denounce with righteous indignation
-                    dislike men who are so beguiled demoralized by the…
+                    Establish and grow your business credit profile using your
+                    EIN, without a personal guarantee (PG) or SSN. This allows
+                    you to qualify for high-limit credit cards, lines of credit,
+                    and funding, independent of your personal credit. Whether
+                    you’re forming a new LLC or already have one, we guide you
+                    through every step to ensure your business builds strong
+                    credit and gains access to financial opportunities.
                   </p>
                   <a href="#" className="link__go">
                     Read more
@@ -49,7 +54,7 @@ function BlogSoftware() {
                   />
                   <div className="contnet_a">
                     <p>POSTED BY</p>
-                    <h4>Creote Elements</h4>
+                    <h4>DNA Lions</h4>
                   </div>
                 </div>
               </div>
@@ -65,7 +70,7 @@ function BlogSoftware() {
                 />
                 <a href="#" className="categories">
                   <i className="icon-folder" />
-                  EMPLOYEE RELATIONS
+                  SERVICES
                 </a>
               </div>
               <div className="content_box">
@@ -75,12 +80,16 @@ function BlogSoftware() {
                 <div className="source">
                   <h2 className="title">
                     <a href="blog-single.html" rel="bookmark">
-                      How to Handle Your Good Employee
+                      Entity Formation
                     </a>
                   </h2>
                   <p className="short_desc">
-                    On the other hand, we denounce with righteous indignation
-                    dislike men who are so beguiled demoralized by the…
+                    Choosing the right business structure is crucial for tax
+                    advantages and legal protection. We handle: LLC formation
+                    and business registration, Obtaining an EIN (Employer
+                    Identification Number), Setting up state tax accounts for
+                    compliance, & Advising on the best entity type for your
+                    needs (LLC, S-Corp, C-Corp).
                   </p>
                   <a href="#" className="link__go">
                     Read more
@@ -96,7 +105,7 @@ function BlogSoftware() {
                   />
                   <div className="contnet_a">
                     <p>POSTED BY</p>
-                    <h4>Creote Elements</h4>
+                    <h4>DNA Lions</h4>
                   </div>
                 </div>
               </div>
@@ -114,7 +123,7 @@ function BlogSoftware() {
                 />
                 <a href="#" className="categories">
                   <i className="icon-folder" />
-                  EMPLOYEE RELATIONS
+                  SERVICES
                 </a>
               </div>
               <div className="content_box">
@@ -124,12 +133,16 @@ function BlogSoftware() {
                 <div className="source">
                   <h2 className="title">
                     <a href="blog-single.html" rel="bookmark">
-                      Why Should Business Payroll Outsourcing?
+                      Individual & Business Taxes
                     </a>
                   </h2>
                   <p className="short_desc">
-                    On the other hand, we denounce with righteous indignation
-                    dislike men who are so beguiled demoralized by the…
+                    Stay compliant and maximize your savings with our expert tax
+                    services. We offer: Tax preparation and filing for
+                    individuals and businesses, Strategic tax planning to reduce
+                    liabilities, Guidance on deductions, exemptions, and tax
+                    credits, & Assistance with IRS audits, tax disputes, and
+                    compliance.
                   </p>
                   <a href="#" className="link__go">
                     Read more
@@ -145,7 +158,164 @@ function BlogSoftware() {
                   />
                   <div className="contnet_a">
                     <p>POSTED BY</p>
-                    <h4>Creote Elements</h4>
+                    <h4>DNA Lions</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid_box _card">
+            <div className="news_box default_style list_view has_images">
+              <div className="image img_hover-1">
+                <img
+                  src="../assets/images/cal-to-action.jpg"
+                  className="img-fluid"
+                  alt="img"
+                />
+                <a href="#" className="categories">
+                  <i className="icon-folder" />
+                  SERVICES
+                </a>
+              </div>
+              <div className="content_box">
+                <div className="date">
+                  <span className="date_in_number">Jan 12 , 2023</span>
+                </div>
+                <div className="source">
+                  <h2 className="title">
+                    <a href="blog-single.html" rel="bookmark">
+                      Bookkeeping & Accounting
+                    </a>
+                  </h2>
+                  <p className="short_desc">
+                    Accurate financial records are essential for business
+                    success. Our team provides: Bookkeeping services to track
+                    income, expenses, and cash flow, Financial reporting and
+                    analysis for better decision-making, Payroll management to
+                    ensure timely and accurate payments, & Budgeting and
+                    forecasting to help your business grow.
+                  </p>
+                  <a href="#" className="link__go">
+                    Read more
+                  </a>
+                </div>
+                <div className="auhtour_box">
+                  <img
+                    alt="img"
+                    className="img-fluid"
+                    src="assets/images/auth-2.png"
+                    height={60}
+                    width={60}
+                  />
+                  <div className="contnet_a">
+                    <p>POSTED BY</p>
+                    <h4>DNA Lions</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid_box _card">
+            <div className="news_box default_style list_view has_images">
+              <div className="image img_hover-1">
+                <img
+                  src="../assets/images/cal-to-action-2.jpg"
+                  className="img-fluid"
+                  alt="img"
+                />
+                <a href="#" className="categories">
+                  <i className="icon-folder" />
+                  SERVICES
+                </a>
+              </div>
+              <div className="content_box">
+                <div className="date">
+                  <span className="date_in_number">Jan 12 , 2023</span>
+                </div>
+                <div className="source">
+                  <h2 className="title">
+                    <a href="blog-single.html" rel="bookmark">
+                      Credit Repair
+                    </a>
+                  </h2>
+                  <p className="short_desc">
+                    Boost your credit score and improve your financial standing.
+                    Our services include: Reviewing your credit reports for
+                    errors and inaccuracies, Disputing negative items with
+                    credit bureaus and creditors, Creating a customized debt
+                    reduction plan, & Helping you access better loan and credit
+                    options.
+                  </p>
+                  <a href="#" className="link__go">
+                    Read more
+                  </a>
+                </div>
+                <div className="auhtour_box">
+                  <img
+                    alt="img"
+                    className="img-fluid"
+                    src="assets/images/auth-1.png"
+                    height={60}
+                    width={60}
+                  />
+                  <div className="contnet_a">
+                    <p>POSTED BY</p>
+                    <h4>DNA Lions</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid_box _card">
+            <div className="news_box default_style list_view has_images">
+              <div className="image img_hover-1">
+                <img
+                  width={1920}
+                  height={1052}
+                  src="../assets/images/expertise-bg-1.jpg"
+                  className="img-fluid"
+                  alt="img"
+                />
+                <a href="#" className="categories">
+                  <i className="icon-folder" />
+                  SERVICES
+                </a>
+              </div>
+              <div className="content_box">
+                <div className="date">
+                  <span className="date_in_number">Jan 12 , 2023</span>
+                </div>
+                <div className="source">
+                  <h2 className="title">
+                    <a href="blog-single.html" rel="bookmark">
+                      Tax Consulting Services
+                    </a>
+                  </h2>
+                  <p className="short_desc">
+                    We assist entrepreneurs and businesses with essential legal
+                    and tax requirements, including: Business registration &
+                    name filing, Obtaining a Federal Employer Identification
+                    Number (FEIN), State tax setup, including sales tax,
+                    employee withholding, and unemployment tax, Assistance with
+                    professional licenses, business permits, and certifications,
+                    & Guidance on becoming a SWAM-certified business (Small,
+                    Woman-Owned, Minority-Owned).
+                  </p>
+                  <a href="#" className="link__go">
+                    Read more
+                  </a>
+                </div>
+                <div className="auhtour_box">
+                  <img
+                    alt="img"
+                    className="img-fluid"
+                    src="assets/images/auth-2.png"
+                    height={60}
+                    width={60}
+                  />
+                  <div className="contnet_a">
+                    <p>POSTED BY</p>
+                    <h4>DNA Lions</h4>
                   </div>
                 </div>
               </div>
